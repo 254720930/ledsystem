845b15e5f75c3863e5441e0f9e175b9970d06fe7
yangaimin <13103825148@136.com>
2019-08-01 10:05:29 +0800

    上传项目架构

---
+++ src/main/java/com/xcy/LedsystemApplication.java
@@ -0,0 +1,13 @@
+package com.xcy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LedsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xcy/ledsystem/LedsystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xcy.ledsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LedsystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
